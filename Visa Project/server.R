f13830e9f525f97ff1feb006e3ef8820654c7c87
NickM <nmaloof925@gmail.com>
2017-10-10 20:38:09 -0400

    First Commit All shiny files

---
+++ Visa Project/server.R
@@ -0,0 +1,135 @@
+library(data.table)
+library(dplyr)
+
+server <- function(input, output, session){
+    #-----Read the Data Set-----#
+    Visas <- fread("TestVisas.csv")
+    
+    #-----Get the Jobs SOC-----#
+    getJobsSOC <- reactive({
+        term <- toupper(input$JobCategory)
+        UniqTerms <- unique(Visas$SOC_NAME)
+        matched <- UniqTerms[grep(term, UniqTerms)]
+        if(length(matched) > 5){
+            return(matched[1:5])
+        }else{
+            return(matched[1:length(matched)])
+        }
+    })
+    
+    #-----Get The Jobs Title-----#
+    getJobsTitle <- reactive({
+        term <- input$JobTitle
+        return(Visas[grep(term, Visas$JOB_TITLE, ignore.case = TRUE)])
+    })
+    
+    #-----Get The Long/Lat-----#
+    getJobLocat <- reactive({
+        Locat <- Visas[SOC_NAME == input$SpecificJob] %>% select(., lat, lon) %>% 
+            mutate(., latlong = paste(lat, lon, sep = ':'))
+        return(Locat)
+    })
+    
+    #-----Plot All Applicants-----#
+    output$TotalApplicants <- renderPlotly({
+        VD <- Visas[, .(TotalApplicants = .N), by = YEAR]
+        plot_ly(
+            data = VD, x = ~YEAR, y = ~TotalApplicants, type = "scatter",
+            mode = "lines+markers"
+        )
+    })
+    
+    #-----Plot Applicants by Status-----#
+    output$CaseApplicants <- renderPlotly({
+        VD <- Visas[, .(TotalApplicants = .N), by = .(YEAR, CASE_STATUS)]
+        plot_ly(
+            data = VD, x = ~YEAR, y = ~TotalApplicants, color = ~CASE_STATUS,
+            type = "scatter", mode = "lines+markers"
+        )
+    })
+    
+    #-----Plot Applicants by Percentage-----#
+    output$FreqApplicants <- renderPlotly({
+        VD <- Visas %>% group_by(., YEAR, CASE_STATUS) %>% summarise(., count = n())%>%
+            mutate(., Proportion = count/sum(count))
+        VD <- as.data.table(VD)
+        plot_ly(
+            data = VD, x = ~YEAR, y = ~Proportion, color = ~CASE_STATUS,
+            type = "scatter", mode = "lines+markers"
+        )
+    })
+    
+    #-----Output The Job Choices-----#
+    output$JobSOC <- renderUI({
+        radioButtons(inputId = "SpecificJob", label = "Choose a Specific Job Category", choices = getJobsSOC())
+    })
+    
+    #-----Output The Job Titles-----#
+    # output$JobTitles <- renderUI({
+    #     radioButtons(inputId = "SpecJobTitle", label = "Choose a Specific Job Title", choices = getJobsTitle())
+    # })
+    
+    #-----Plot the Output of Jobs SOC-----#
+    output$SOCPlot <- renderPlotly({
+        VD <- Visas[SOC_NAME == input$SpecificJob, .(TotalApplicants = .N), by = YEAR]
+        plot_ly(
+            data = VD, x = ~YEAR, y = ~TotalApplicants, type = "scatter", mode = "lines+markers"
+        )
+    })
+    
+    #-----Plot the Output of Jobs Title-----#
+    output$TitlePlot <- renderPlotly({
+        VD <- getJobsTitle()[, .(TotalApplicants = .N), by = .(YEAR, CASE_STATUS)]
+        plot_ly(
+            data = VD, x = ~YEAR, y = ~TotalApplicants, color = ~CASE_STATUS,
+            type = "scatter", mode = "lines+markers"
+        )
+    })
+    
+    #-----InfoBoxOutputs-----#
+    #---First Half---#
+    output$MinValue <- renderInfoBox({
+        dat <- Visas[(SOC_NAME == input$SpecificJob) & CASE_STATUS == "CERTIFIED"]
+        infoBox("Minimum $", min(dat$PREVAILING_WAGE), icon = icon("minus-square"), color = "purple", fill = T)
+    })
+    output$MaxValue <- renderInfoBox({
+        dat <- Visas[(SOC_NAME == input$SpecificJob) & CASE_STATUS == "CERTIFIED"]
+        infoBox("Maximum $", max(dat$PREVAILING_WAGE), icon = icon("plus-square"), color = "purple", fill = T)
+    })
+    output$MeanValue <- renderInfoBox({
+        dat <- Visas[(SOC_NAME == input$SpecificJob) & CASE_STATUS == "CERTIFIED"]
+        infoBox("Average $", round(mean(dat$PREVAILING_WAGE),2), icon = icon("bullseye"), color = "purple", fill = T)
+    })
+    output$MedianValue <- renderInfoBox({
+        dat <- Visas[(SOC_NAME == input$SpecificJob) & CASE_STATUS == "CERTIFIED"]
+        infoBox("Median $", median(dat$PREVAILING_WAGE), icon = icon("calculator"), color = "purple", fill = T)
+    })
+    #---Second Half---#
+    output$MinValue2 <- renderInfoBox({
+        dat <- getJobsTitle()[CASE_STATUS == "CERTIFIED"]
+        infoBox("Minimum $", min(dat$PREVAILING_WAGE), icon = icon("calculator"), color = "purple", fill = T)
+    })
+    output$MaxValue2 <- renderInfoBox({
+        dat <- getJobsTitle()[CASE_STATUS == "CERTIFIED"]
+        infoBox("Maximum $", max(dat$PREVAILING_WAGE), icon = icon("calculator"), color = "purple", fill = T)
+    })
+    output$MeanValue2 <- renderInfoBox({
+        dat <- getJobsTitle()[CASE_STATUS == "CERTIFIED"]
+        infoBox("Average $", round(mean(dat$PREVAILING_WAGE),2), icon = icon("bullseye"), color = "purple", fill = T)
+    })
+    output$MedianValue2 <- renderInfoBox({
+        dat <- getJobsTitle()[CASE_STATUS == "CERTIFIED"]
+        infoBox("Median $", median(dat$PREVAILING_WAGE), icon = icon("calculator"), color = "purple", fill = T)
+    })
+    
+        
+    #-----Plot the GeoMap-----#
+    output$GeoMap1 <- renderGvis({
+        gvisMap(
+            getJobLocat(), "latlong",
+            options = list(region = "US", displayMode = "Markers", mapType = "normal",
+                           width = "auto", height = "auto")
+        )
+    })
+    
+}
---
+++ Visa Project/ui.R
@@ -0,0 +1,155 @@
+ui <- dashboardPage(
+    skin = "black",
+    ###----------Dashboard Header----------###
+    dashboardHeader(
+        title = "H-1B Visa Applicants"
+    ),
+    ###----------Dashboard Sidebar----------###
+    dashboardSidebar(
+        collapsed = TRUE,
+        sidebarUserPanel(
+            name = "Nicholas Maloof", image = "Nicholas1.jpg"
+        ),
+        sidebarMenu(
+            menuItem(
+                text = "Diagrams", tabName = "GraphTab",
+                icon = icon("area-chart"), badgeLabel = "Charts",
+                badgeColor = "blue"
+            ),
+            menuItem(
+                text = "Specific Jobs", tabName = "SelectTab",
+                icon = icon("id-card"), badgeLabel = "Careers",
+                badgeColor = "red"
+            ),
+            menuItem(
+                text = "Job Locations", tabName = "JobLocatTab",
+                icon = icon("globe"), badgeLabel = "Where",
+                badgeColor = "green"
+            )
+        )
+    ),
+    ###----------Dashboard Body----------###
+    dashboardBody(
+        tags$head(tags$style(HTML("
+        .skin-black .main-header .logo {
+                                  background: gray;
+                                  color: white;
+                                  font-family: Palatino;
+                                  font-size: 20px;
+                                  }
+                                  .skin-black .main-header .logo:hover {
+                                  background: rgb(0,0,0);
+                                  }
+                                  .skin-black .left-side, .skin-black .main-sidebar, .skin-black .wrapper{
+                                  background: gray;
+                                  }
+                                  .skin-black .main-header .navbar{
+                                  background: -webkit-linear-gradient(left, gray , black);
+                                  }
+                                  .skin-black .sidebar a{
+                                  font-family: CenturyGothic;
+                                  font-size: 15px;
+                                  }
+                                  p{
+                                  font-family: cursive;
+                                  font-size: 17px;
+                                  }
+                                  h1{
+                                  font-family: Lobster;
+                                  font-family: italic;
+                                  }"))),
+        tabItems(
+            #-----Tab Item 1 Begins-----#
+            tabItem(
+                tabName = "GraphTab",
+                fluidPage(
+                    h1("A Look at Visas Through the Years"),
+                    tabBox(
+                        title = tagList(shiny::icon("line-chart"), "Visa Applicants"),
+                        side = "left",
+                        width = 12,
+                        tabPanel(
+                            title = "All",
+                            plotlyOutput(outputId = "TotalApplicants")
+                        ),
+                        tabPanel(
+                            title = "Status",
+                            plotlyOutput(outputId = "CaseApplicants")
+                        ),
+                        tabPanel(
+                            title = "Freq",
+                            plotlyOutput(outputId = "FreqApplicants")
+                        )
+                    )
+                )
+            ),
+            #-----Tab Item 1 Ends-----#
+            
+            #-----Tab Item 2 Begins-----#
+            tabItem(
+                tabName = "SelectTab",
+                fluidPage(
+                    h1("Searching for the Job You Want"),
+                    fluidRow(
+                        tabBox(
+                            title = tagList(shiny::icon("search"), "Job Title Categories"),
+                            side = "left",
+                            width = 12,
+                            height = 850,
+                            tabPanel(
+                                title = "Standard Occupation",
+                                column(
+                                    width = 6,
+                                    textInput(
+                                        inputId = "JobCategory", label = "Please Enter a Job Category",
+                                        placeholder = "Insert Job Category", width = "100%"
+                                    )
+                                ),
+                                column(
+                                    width = 6,
+                                    uiOutput(outputId = "JobSOC")
+                                ),
+                                br(),br(),br(),br(),br(),br(),br(),br(),
+                                plotlyOutput(outputId = "SOCPlot"),
+                                br(),
+                                infoBoxOutput(outputId = "MinValue"),
+                                infoBoxOutput(outputId = "MaxValue"),
+                                infoBoxOutput(outputId = "MeanValue"),
+                                infoBoxOutput(outputId = "MedianValue")
+                            ),
+                            tabPanel(
+                                title = "Job Title",
+                                column(
+                                    width = 12,
+                                    textInput(
+                                        inputId = "JobTitle", label = "Please Enter a Job Title",
+                                        placeholder = "Insert Job Title", width = "100%"
+                                    )
+                                ),
+                                br(),br(),br(),br(),
+                                plotlyOutput(outputId = "TitlePlot"),
+                                br(),
+                                infoBoxOutput(outputId = "MinValue2"),
+                                infoBoxOutput(outputId = "MaxValue2"),
+                                infoBoxOutput(outputId = "MeanValue2"),
+                                infoBoxOutput(outputId = "MedianValue2")
+                            )
+                        )
+                    )
+                )
+            ),
+            #-----Tab Item 2 Ends-----#
+            
+            
+            #-----Tab Item 3 Begins-----#
+            tabItem(
+                tabName = "JobLocatTab",
+                fluidPage(
+                    h1("Where Are You Going to Work"),
+                    htmlOutput(outputId = "GeoMap1")
+                )
+            )
+            #-----Tab Item 3 Ends-----#
+        )
+    )
+)
